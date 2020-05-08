28912603b4e229e0c3e16e6cd93bb9ec072d6790
Maciej <darkuso@o2.pl>
2020-05-08 15:22:07 +0200

    almost working?

--- Task 2/MLP.py
+++ Task 2/MLP.py
@@ -1,2 +1,36 @@
 import pandas as pd
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+from sklearn.metrics import mean_squared_error
 
+
+def parse_data(file_path):
+    df = pd.read_csv(file_path)
+    return df[['measurement x', 'measurement y', 'reference x', 'reference y']]
+
+
+data = parse_data('./dataset.csv')
+input_columns = ['measurement x', 'measurement y']
+target_columns = ['reference x', 'reference y']
+x = []
+y = []
+for it in range(data[input_columns[0]].__len__()):
+    x.append([data[input_columns[0]][it], data[input_columns[1]][it]])
+    y.append([data[target_columns[0]][it], data[target_columns[1]][it]])
+
+x_train, x_test = x[:-1541], x[-1540:]
+y_train, y_test = y[:-1541], y[-1540:]
+
+scaler = StandardScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+mlp = MLPRegressor(hidden_layer_sizes=(2, 2, 2), activation='relu', solver='adam', max_iter=400)
+mlp.fit(x_train, y_train)
+
+predict_train = mlp.predict(x_train)
+print(np.sqrt(mean_squared_error(y_train, predict_train)))
+predict_test = mlp.predict(x_test)
+print(np.sqrt(mean_squared_error(y_test, predict_test)))
--- Task 2/excel.py
+++ Task 2/excel.py
@@ -2,25 +2,17 @@ import os
 import pandas as pd
 from natsort import natsorted
 
-
 dirName = './pozyxAPI_dane_pomiarowe'
 
 
-def parselearndata():
-    learndata = pd.DataFrame()
+def parse_learn_data():
+    data = pd.DataFrame()
     for filename in natsorted(os.listdir(dirName)):
         if filename.endswith(".xlsx"):
             df = pd.read_excel(f"{dirName}/{filename}")
-            df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-            learndata = learndata.append(df, ignore_index=True)
-    print(learndata.head())
-    learndata.to_csv("./learn_data.csv")
-
-
-def parsetestdata():
-    dataframe = pd.DataFrame()
+            df = df[['0/timestamp', 't', 'no', 'measurement x', 'measurement y', 'reference x', 'reference y']]
+            data = data.append(df, ignore_index=True)
     df = pd.read_excel("./pozyxAPI_only_localization_dane_testowe_i_dystrybuanta.xlsx")
-    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-    dataframe = dataframe.append(df, ignore_index=True)
-    dataframe.to_csv("./test_data.csv")
-
+    df = df[['0/timestamp', 't', 'no', 'measurement x', 'measurement y', 'reference x', 'reference y']]
+    data = data.append(df, ignore_index=True)
+    data.to_csv("./dataset.csv")
